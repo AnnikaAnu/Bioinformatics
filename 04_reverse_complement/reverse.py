b7083ae0dba85efc19d00c43780a754c9e60a0ea
Annika Aamer <annika.aamer@gmx.de>
2026-04-21 16:12:47 +0200

    Create reverse.py

---
+++ 04_reverse_complement/reverse.py
@@ -0,0 +1,8 @@
+# Reverse a DNA string
+def Reverse(Pattern):
+    rev = ""                   # initialize empty string
+    for char in Pattern:       # iterate over each nucleotide
+        rev = char + rev       # add nucleotide to front of string
+    return rev                 # return reversed DNA string
+        
+print(Reverse("AAAACCCGGT"))   # TGGCCCAAAA
