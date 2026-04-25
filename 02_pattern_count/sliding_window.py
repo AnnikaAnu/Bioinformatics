2978346846fb22b95be0075e71cef954afa3bce1
Annika Aamer <annika.aamer@gmx.de>
2026-04-25 11:05:13 +0200

    Update sliding_window.py

--- 02_pattern_count/sliding_window.py
+++ 02_pattern_count/sliding_window.py
@@ -1,10 +1,10 @@
 # Count the occurrences of a k-mer (pattern) in a  DNA sequence (text)
 
-def pattern_count(text, pattern):                    
+def PatternCount(Text, Pattern):                    
     count = 0                                        # initialize counter
-    for i in range(len(text)-len(pattern)+1):        # slide window across sequence
-        if text[i:i+len(pattern)] == pattern:        # k-mer match found
+    for i in range(len(Text)-len(Pattern)+1):        # slide window across sequence
+        if Text[i:i+len(Pattern)] == Pattern:        # k-mer match found
             count +=1                                # increment counter
     return count                                     # return total match count
   
-print(pattern_count("GCGCG", "GCG"))                 # sample: GCG appears twice
+print(PatternCount("GCGCG", "GCG"))                 # sample: GCG appears twice
