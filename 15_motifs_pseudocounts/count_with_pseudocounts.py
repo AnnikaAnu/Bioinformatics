f2092107b1f793fd55bcce9e9a12e09fe2f64d77
Annika Aamer <annika.aamer@gmx.de>
2026-08-01 13:22:47 +0200

    Create count_with_pseudocounts.py

---
+++ 15_motifs_pseudocounts/count_with_pseudocounts.py
@@ -0,0 +1,37 @@
+def  CountWithPseudocounts(Motifs):
+    """Count nucleotide occurrences per column of Motifs with pseudocounts added."""
+    count = {}
+    
+    t = len(Motifs)                       # number of motifs (rows)
+    k = len(Motifs[0])                    # length of each motif (number of columns)
+    
+    for symbol in "ACGT":                 # initialize with pseudocount instead of zero
+        count[symbol] = []
+        for j in range(k):                
+            count[symbol].append(1)      
+
+    for i in range(t):                    # fill count matrix
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] +=1          # increment count for nucleotide at column j
+
+    return count
+
+
+Motifs = ["AACGTA", "CCCGTT", "CACCTT", "GGATTA", "TTCCGG"]
+
+
+def Profile(Motifs):
+    """Return the profile matrix of Motifs as nucleotide frequencies per column."""
+   
+    count = CountWithPseudocounts(Motifs)                        # get nucleotide count matrix (with pseudocounts)
+    t = sum(count[symbol][0] for symbol in "ACGT")               # column sum after pseudocounts
+    k = len(Motifs[0])                                           # length of each motif (number of columns)
+    profile = {}
+    for symbol in "ACGT":
+            profile[symbol] = []                                 # initialize empty list for each nucleotide
+            for j in range(k):
+                profile[symbol].append(count[symbol][j] / t)     # divide count by total rows incl. pseudocounts to get frequency
+    return profile
+
+print (CountWithPseudocounts(Motifs))
